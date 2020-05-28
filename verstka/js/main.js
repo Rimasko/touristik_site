5f7d92d69d4d2b008e5efbdffb9fbea3f3c4bc11
samir <rimas.amga@gmail.com>
2020-05-28 18:55:51 +0700

    Добавлена главная страница

--- verstka/js/main.js
+++ verstka/js/main.js
@@ -1,17 +1,16 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-  if(document.getElementById("mySidenav").style.width == "0px") {
-    console.log("WDD");
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  else {
-    closeNav();
-  }
+    if (document.getElementById("mySidenav").style.width == "0px") {
+        console.log("WDD");
+        document.getElementById("mySidenav").style.width = "250px";
+    } else {
+        closeNav();
+    }
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "0";
 }
 
 $('.dropdown-toggle').dropdown()
