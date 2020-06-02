fc00e67e59362063663664065e3824967d8ab114
samir <rimas.amga@gmail.com>
2020-06-02 12:38:56 +0700

    Добавлен проект Django

---
+++ ekvatour/static/images/base page_files/main.js
@@ -0,0 +1,15 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  if(document.getElementById("mySidenav").style.width == "0px") {
+    console.log("WDD");
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  else {
+    closeNav();
+  }
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
