e6ed85e63c827b461821a56d2671846f0319bab0
samir <rimas.amga@gmail.com>
2020-06-07 13:55:04 +0700

    fix

--- ekvatour/config/settings.py
+++ ekvatour/config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -129,8 +129,6 @@ if ENVIRONMENT == 'production':
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    import dj_database_url
-
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES = {'default': db_from_env}
 
